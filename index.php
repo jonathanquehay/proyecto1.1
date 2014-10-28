fc25e59f17ca811c40ac1dfe1e2f3c8fc9cad57e
Jonathán Moreno <jjmore83@gmail.com>
2014-10-28 17:53:47 -0600

    probando codigo

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <title>Proyecto 1:</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/jquery-ui.css" rel="stylesheet">	
+  </head>
+  <body>
+ 
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-ui.js"></script> 	
+    
+  </body>
+</html>
